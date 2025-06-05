3071ce660568fba8c4dd3332c384b8805c5edb96
danielrduque <124942165+danielrduque@users.noreply.github.com>
2025-06-05 18:22:18 -0500

    despliegue

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+// src/environments/environment.prod.ts
+export const environment = {
+  production: true,
+  apiUrl: 'https://backend-inqtel.vercel.app/', // <--- ¡CAMBIA ESTO!
+  // Otras variables de entorno si las tienes
+};
